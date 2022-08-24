dce0abe0507cb03a8c0572f37786df3361f3cef4
Ilia Fonderkin <58978371+fonderk@users.noreply.github.com>
2022-08-24 19:46:12 +0300

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -99,7 +99,6 @@ function renderPosts(response){
         postBody.innerHTML=el.title;
         post.appendChild(postBody);
     })
-    console.log(response);
 }
 
 // render posts and add comments
